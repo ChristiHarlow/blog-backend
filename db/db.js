7eba425293e9d61b4b25fdd0ab9aa8ee44d3002e
Christi Harlow <cah120671@gmail.com>
2022-08-17 18:50:38 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,13 +1,32 @@
 //#3 setup DB models
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://christiharlow@localhost:5432/blog", {
-    logging: false,
-});
+let options = {};
+let databaseURL = process.env.DATABASE_URL;
+if (!databaseURL) {
+    // this means we're on localhost!
+    databaseURL = "postgres://hackupstate@localhost:5432/blog";
+    options = {
+        logging: false,
+    };
+} else {
+    // we're not on localhost
+    options = {
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    };
+}
+
+const db = new Sequelize(databaseURL, options);
 const User = require("./User")(db);
 const Post = require("./Post")(db);
 
-//#5
+//#5 connect and sync to DB
 const connectToDB = async () => {
     try {
         await db.authenticate();
@@ -17,8 +36,6 @@ const connectToDB = async () => {
         console.error(error);
         console.error("PANIC! DB PROBLEMS!");
     }
-
-    Post.belongsTo(User, { foreignKey: "authorID" });
 };
 
 connectToDB();
